44cdbb840a9485a8f450abb3664e585be1dc7c72
Maxi Cabello <maxicabello09@gmail.com>
2022-10-12 23:40:30 -0300

    Agrego el proyecto

---
+++ index.js
@@ -0,0 +1,6 @@
+const navtoggle = document.querySelector(".nav-toggle")
+const navmenu = document.querySelector(".nav-menu")
+
+navtoggle.addEventListener("click", () =>{
+navmenu.classList.toggle("nav-menu_visible");
+});
